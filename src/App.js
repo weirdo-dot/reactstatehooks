539e2e83725f7b2b60f4186e317e0c3a3c857baf
weirdo-dot <africanweirdoblog@gmail.com>
2024-06-08 11:48:00 +0100

    run

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'Akeju Mariam',
+        bio: 'A software developer from Nigeria.',
+        imgSrc: 'https://avatars.githubusercontent.com/u/159588898?v=4',
+        profession: 'Software Engineer'
+      },
+      shows: false,
+      lastMountedTime: Date.now(),
+      timeSinceMounted: 0
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ timeSinceMounted: Math.floor((Date.now() - this.state.lastMountedTime) / 1000) });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow = () => {
+    this.setState((prevState) => ({ shows: !prevState.shows }));
+  };
+
+  render() {
+    const { person, shows, timeSinceMounted } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>
+          {shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+
+        {shows && (
+          <div className="profile">
+            <img src={person.imgSrc} alt={person.fullName} />
+            <h1>{person.fullName}</h1>
+            <p>{person.bio}</p>
+            <h2>{person.profession}</h2>
+          </div>
+        )}
+
+        <p>Time since last mount: {timeSinceMounted} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
